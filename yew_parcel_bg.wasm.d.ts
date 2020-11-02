f75ae1099e23f074837578d09404d357c3e0c450
sowetocon <sowetocon@gmail.com>
2020-11-02 23:49:01 +0200

    Updates

---
+++ yew_parcel_bg.wasm.d.ts
@@ -0,0 +1,30 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function run(): void;
+export function __wbgt_should_create_a_container_0(a: number): void;
+export function __wbgt_should_set_style_prop_5(a: number): void;
+export function __wbgt_should_generate_random_string_6(a: number): void;
+export function __wbgt_should_create_item_1(a: number): void;
+export function __wbgt_should_create_clickable_item_2(a: number): void;
+export function __wbgt_should_create_navbar_item_3(a: number): void;
+export function __wbgt_should_create_clickable_navbar_item_4(a: number): void;
+export function __wbg_wasmbindgentestcontext_free(a: number): void;
+export function wasmbindgentestcontext_new(): number;
+export function wasmbindgentestcontext_args(a: number, b: number, c: number): void;
+export function wasmbindgentestcontext_run(a: number, b: number, c: number): number;
+export function __wbgtest_console_log(a: number): void;
+export function __wbgtest_console_debug(a: number): void;
+export function __wbgtest_console_info(a: number): void;
+export function __wbgtest_console_warn(a: number): void;
+export function __wbgtest_console_error(a: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hd948b7f928335619(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8f764103ca8bb6bb(a: number, b: number, c: number): void;
+export function wasm_bindgen__convert__closures__invoke0_mut__h20c6a552ec231782(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_free(a: number, b: number): void;
+export function wasm_bindgen__convert__closures__invoke3_mut__hb349e84778846a63(a: number, b: number, c: number, d: number, e: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h8abe01762bb28f7a(a: number, b: number, c: number, d: number): void;
