56b93997705ba662e1f84f487728d495bb1fe320
sowetocon <sowetocon@gmail.com>
2021-04-19 12:17:42 +0200

    schedule update

--- crate/src/pages/schedule.rs
+++ crate/src/pages/schedule.rs
@@ -29,11 +29,11 @@ impl Component for Schedule {
                     <h2>{"Schedule"}</h2>
                 </Item>
                 <Item layouts=vec!(ItemLayout::ItXs(12))>
-                <dl>{"The conference starts on the 23rd until the 24th April 2021."}</dl>
+                //<dl>{"The conference starts on the 23rd until the 24th April 2021."}</dl>
                 <dt><b>{"Friday: Day 1"}</b></dt>
                 <p>{"[+] 11:00: Keynote ->  On love, health and Ubuntu"}</p>
-                <p>{"[+] 11:40: Entrepreneurship ->  The cyber hustlers startup guide "}</p>
-                <p>{"[+] 12:30: Lunch"}</p>
+                <p>{"[+] 11:30: Entrepreneurship ->  The cyber hustlers startup guide "}</p>
+                <p>{"[+] 12:30: Break"}</p>
                 <p>{"[+] 13:30: Artificial Intelligence -> Guide to building your frst model"}</p>
                 <p>{"[+] 14:20: Blockchain -> Understanding Smart contracts, Defi & NFTs"}</p>
                 <p>{"[+] 15:30: Trading -> From zero to quant"}</p>
@@ -41,7 +41,7 @@ impl Component for Schedule {
                 <dt><b>{"Saturday: Day 2"}</b></dt>
                 <p>{"[+] 11:00: SecIntel -> Hackers and Spies - A comparison"}</p>
                 <p>{"[+] 11:40: SecDevOps -> Agile Threat Modelling"}</p>
-                <p>{"[+] 12:30: Lunch"}</p>
+                <p>{"[+] 12:30: Break"}</p>
                 <p>{"[+] 13:30: Programming -> A look at Rust security"}</p>
                 <p>{"[+] 14:20: Hacking -> Whitebox fuzzing Rust and C/CPP APIs"}</p>
                 <p>{"[+] 15:30: Hacking -> Whitebox web hacking PHP and Java Apps"}</p>
